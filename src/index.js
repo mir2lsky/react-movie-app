de9b8ce1f0dd6ef571c8ab5e21633cc00cca44c0
mir2lsky <mir2lsky@gmail.com>
2024-11-17 00:23:15 +0900

    router modify2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,8 +8,6 @@ root.render(
   // console.log를 찍으면 두번 찍히는데 개발서버에서만 그렇고 프로덕션에서는 한번만 찍힘
   // 보기 싫으면 아래 StrictMode를 주석처리 하면 1번만 찍힘
   // <React.StrictMode>
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <App />
-  </BrowserRouter>
+  <App />
   // </React.StrictMode>
 );
